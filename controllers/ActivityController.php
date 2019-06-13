2290b10f4489aa9928793c3a6ccb55d240f374ce
lyoulka <lyoulka@mail.ru>
2019-06-13 14:32:54 +0400

    From the 3d lesson

--- controllers/ActivityController.php
+++ controllers/ActivityController.php
@@ -1,18 +1,29 @@
 <?php
-
-
 namespace app\controllers;
-
-
 use app\base\BaseWebController;
 use app\controllers\actions\ActivityCreateAction;
-
+use yii\helpers\ArrayHelper;
+use yii\helpers\StringHelper;
 class ActivityController extends BaseWebController
 {
-  public function actions()
-  {
-      return [
-          'create' => ['class' => ActivityCreateAction::class]
-      ];
-  }
+    public function actions()
+    {
+        return [
+            'create' => ['class' => ActivityCreateAction::class]
+        ];
+    }
+    public function actionHelper(){
+        $arr1=['ones'=>'val1','tho'=>['treee'=>'val2']];
+        $val=ArrayHelper::getValue($arr1,'one','def');
+        $val1=ArrayHelper::getValue($arr1,'tho.treee');
+        $data=[['id'=>2,'name'=>'one','phone'=>'888'],['id'=>3,'name'=>'two','phone'=>'888']];
+        $values=ArrayHelper::map($data,'id',function($data){
+            return ArrayHelper::getValue($data,'name').' ('.
+                ArrayHelper::getValue($data,'phone').')';
+        });
+        echo $val;
+        echo '</br>'.$val1;
+//      exit;
+        return $this->render('helper',['values'=>$values]);
+    }
 }
--- controllers/actions/ActivityCreateAction.php
+++ controllers/actions/ActivityCreateAction.php
@@ -3,7 +3,8 @@ namespace app\controllers\actions;
 use app\components\ActivityComponent;
 use app\models\Activity;
 use yii\base\Action;
-
+use yii\web\Response;
+use yii\widgets\ActiveForm;
 class ActivityCreateAction extends Action
 {
     public function run(){
@@ -13,14 +14,15 @@ class ActivityCreateAction extends Action
             'modelClass' => 'app\models\Activity']);
         if (\Yii::$app->request->isPost){
             $model->load(\Yii::$app->request->post());
-           if(\Yii::$app->activity->createActivity($model)){
-               echo 'OK';
-               exit;
-           }
+            if(\Yii::$app->request->isAjax){
+                \Yii::$app->response->format=Response::FORMAT_JSON;
+                return ActiveForm::validate($model);
+            }
+            if(\Yii::$app->activity->createActivity($model)){
+                return $this->controller->render('view',['model'=>$model]);
+            }
         }
-        $url = $model -> getLink();
-        return  $this -> controller -> render('create', ['name' => 'Юлия', 'model'=>$model, 'url' => $url]);
 
-
+        return  $this -> controller -> render('create', ['name' => 'Юлия', 'model'=>$model]);
     }
 }
